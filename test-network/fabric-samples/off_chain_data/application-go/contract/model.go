3a970afc2b03cb9ade67c466e031d0a2b94c8072
viditkulsh <viditkul08@gmail.com>
2025-08-12 11:54:01 +0530

    Modified the  project directory

--- test-network/fabric-samples/off_chain_data/application-go/contract/model.go
+++
@@ -1,9 +0,0 @@
-package contract
-
-type Asset struct {
-	ID             string `json:"ID"`
-	Color          string `json:"Color"`
-	Size           uint64 `json:"Size"`
-	Owner          string `json:"Owner"`
-	AppraisedValue uint64 `json:"AppraisedValue"`
-}
